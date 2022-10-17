7fd3da9af42f227ce2dc00baa1e1e7333b30215c
CodeImm <45065137+CodeImm@users.noreply.github.com>
2022-10-17 10:37:10 +0700

    chore: ts for i18next

--- src/types/react-i18next.d.ts
+++ src/types/react-i18next.d.ts
@@ -1,7 +1,8 @@
 import { resources } from '../i18n/config';
 
-// react-i18next versions lower than 11.11.0
+// react-i18next versions higher than 11.11.0
 declare module 'react-i18next' {
-  type DefaultResources = typeof resources['ru'];
-  interface Resources extends DefaultResources {}
+  interface CustomTypeOptions {
+    resources: typeof resources['ru'];
+  }
 }
